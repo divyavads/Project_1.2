78b5c13468fb6df16b881a0c0adc465e59217f98
Divya Vaddavalli <divyavaddavalli@Divyas-MacBook-Pro.local>
2025-04-24 17:40:40 -0700

    fix whitespace

--- sshell.c
+++ sshell.c
@@ -34,7 +34,7 @@ void check_bg_jobs(int *bg_ct, pid_t bg_pids[], int bg_retvals[], char bg_cmd[])
         }
     }
     if (all_done) {
-        fprintf(stderr, "+ completed '%s'", bg_cmd);
+        fprintf(stderr, "+ completed '%s' ", bg_cmd);
         for (int i = 0; i < *bg_ct; i++) {
             fprintf(stderr, "[%d]", bg_retvals[i]);
         }
